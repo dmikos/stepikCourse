6f8a3db0f4e3c0029cb34ca65054538976fc00dc
Dmitriy Kos <dkostinov@gmail.com>
2016-09-27 17:45:00 +0300

    .

--- stepik-512_24461-step9.py
+++ stepik-512_24461-step9.py
@@ -5,12 +5,29 @@
 class Buffer:
     def __init__(self):
         # конструктор без аргументов
-
+        self.part = []
 
     def add(self, *a):
         # добавить следующую часть последовательности
-
+        self.part.extend(a)
+        while len(self.part) >= 5:
+            count = 0
+            for i in range(5):
+                count += self.part.pop(0)
+            print(count)
 
     def get_current_part(self):
         # вернуть сохраненные в текущий момент элементы последовательности в порядке, в котором они были
         # добавлены
+        print(self.part)
+
+###
+buf = Buffer()
+buf.add(1, 2, 3)
+buf.get_current_part() # вернуть [1, 2, 3]
+buf.add(4, 5, 6) # print(15) – вывод суммы первой пятерки элементов
+buf.get_current_part() # вернуть [6]
+buf.add(7, 8, 9, 10) # print(40) – вывод суммы второй пятерки элементов
+buf.get_current_part() # вернуть []
+buf.add(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1) # print(5), print(5) – вывод сумм третьей и четвертой пятерки
+buf.get_current_part() # вернуть [1]
