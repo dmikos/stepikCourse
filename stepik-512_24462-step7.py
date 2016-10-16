c43e01a5d0ca92f3bb81c2ba705d9b04328e60e9
Dmitriy Kostinov <dkostinov@gmail.com>
2016-10-16 14:42:30 +0300

    .

--- stepik-512_24462-step7.py
+++ stepik-512_24462-step7.py
@@ -1,24 +1,19 @@
 #!/usr/bin/env python3
 
 # https://stepik.org/lesson/%D0%9D%D0%B0%D1%81%D0%BB%D0%B5%D0%B4%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5-%D0%BA%D0%BB%D0%B0%D1%81%D1%81%D0%BE%D0%B2-24462/step/7?course=Python-%D0%BE%D1%81%D0%BD%D0%BE%D0%B2%D1%8B-%D0%B8-%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D0%BD%D0%B5%D0%BD%D0%B8%D0%B5&unit=6768
+version__ = "$Revision: 201610161442 $"
+# $Source$
 
-def create(ns, parent):
-    # print("request = create", "namespace =", ns, ", parent =", parent)
-    templist = list()
-    templist.append(parent)
-    Struct[ns] = templist
+
+def add(C1, C2):
+    print("C1 =", C1,"C2 =", C2)
+    #templist = list(Struct.get(ns))
+    #templist.append(var)
+    #Struct[ns] = templist
     # print(Struct)
 
 
-def add(ns, var):
-    # print("request = add", "namespace =", ns, ", var =", var)
-    templist = list(Struct.get(ns))
-    templist.append(var)
-    Struct[ns] = templist
-    # print(Struct)
-
-
-def get(ns, var):
+def get(C1, C2):
     # print("request = get,", "namespace =", ns, ", var =", var)
     while not(var in Struct[ns]):
         ns = list(Struct.get(ns))[0]
@@ -26,19 +21,21 @@ def get(ns, var):
             break
     return ns
 
-# num = int(input("Enter number: "))
-num = int(input())
-Struct = {'global': [None]}
+i_num = int(input("Enter number: "))
+# i_num = int(input())
+Struct = {}
 # print(Struct)
 
-for i in range(num):
-    v1, v2, v3 = str(input()).split()
-    if v1 == "create":
-        create(v2, v3)
-    elif v1 == "add":
-        add(v2, v3)
-    elif v1 == "get":
-        # get(v2, v3)
-        print(get(v2, v3))
+for i in range(i_num):
+    string = str(input())
+    if len(string) == 1:
+        C1 = string
+        C2 = 'object'
+        add(C1, C2)
+    else:
+        C1 = string.split()[0]
+        C2 = string.split()[-1]
+        add(C1, C2)
+
 
 # print(Struct)
