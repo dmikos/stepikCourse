57aba45542990f23b536e0936a7972d8fa5a2cb5
Dmitriy Kos <dkostinov@gmail.com>
2016-10-19 14:17:34 +0300

    .

--- stepik-512_24462-step8.py
+++ stepik-512_24462-step8.py
@@ -8,12 +8,16 @@ version__ = "$Revision: 201610181516 $"
 class ExtendedStack(list):
     def sum(self):
         # операция сложения
+        pass
 
     def sub(self):
         # операция вычитания
+        pass
 
     def mul(self):
         # операция умножения
+        pass
 
     def div(self):
         # операция целочисленного деления
+        pass
