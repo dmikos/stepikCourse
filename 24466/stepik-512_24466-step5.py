40f021b3cbfb5299b8b8b17d64d51343aac62be1
Dmitriy Kos <dkostinov@gmail.com>
2016-10-25 14:48:16 +0300

    .

--- 24466/stepik-512_24466-step5.py
+++ 24466/stepik-512_24466-step5.py
@@ -1,6 +1,7 @@
 import datetime
 
-"""
-Вам будут полезны класс datetime.date для хранения даты и класс datetime.timedelta﻿ для прибавления дней к дате.
-"""
-
+y, m, n = str(input()).split()
+y, m, n = int(y), int(m), int(n)
+d = int(input())
+date = datetime.date(y, m, n) + datetime.timedelta(days=d)
+print(date.year, date.month, date.day)
