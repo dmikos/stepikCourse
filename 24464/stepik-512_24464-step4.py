a047c8bb5b240e64a7a8bff92ecfa811ae5bf9c6
Dmitriy Kostinov <dkostinov@gmail.com>
2016-10-28 22:28:07 +0300

    .

--- 24464/stepik-512_24464-step4.py
+++ 24464/stepik-512_24464-step4.py
@@ -1,45 +1,55 @@
 class multifilter:
     def judge_half(pos, neg):
         # допускает элемент, если его допускает хотя бы половина фукнций (pos >= neg)
-        pass
+        return pos >= neg
 
     def judge_any(pos, neg):
         # допускает элемент, если его допускает хотя бы одна функция (pos >= 1)
-        pass
+        return pos >= 1
 
     def judge_all(pos, neg):
         # допускает элемент, если его допускают все функции (neg == 0)
-        pass
+        return neg == 0
 
-#    def __init__(self, iterable, *funcs, judge=judge_any):
+    def __init__(self, iterable, *funcs, judge=judge_any):
         # iterable - исходная последовательность
         # funcs - допускающие функции
         # judge - решающая функция
-#        pass
+        self.iterable = iterable
+        self.funcs = funcs
+        self.judge = judge
+        self.neg = 0
+        self.pos = 0
+        self.i = 0
 
     def __iter__(self):
         # возвращает итератор по результирующей последовательности
         return self
-        pass
 
     def __next__(self):
-        return 0
+        if self.i < len(self.iterable):
+            for i in self.funcs:
+                if i(self.i):
+                    self.pos += 1
+                else:
+                    self.neg += 1
+            if self.judge(self.pos, self.neg):
+                return self.i
+        else:
+            raise StopIteration
 
-x = multifilter()
-print(iter(x))
+# x = multifilter()
+# print(iter(x))
 
-"""if __name__ == '__main__':
+if __name__ == '__main__':
     def mul2(x):
         return x % 2 == 0
-
 
     def mul3(x):
         return x % 3 == 0
 
-
     def mul5(x):
         return x % 5 == 0
-
 
     a = [i for i in range(31)]  # [0, 1, 2, ... , 30]
 
@@ -51,4 +61,3 @@ print(iter(x))
 
     print(list(multifilter(a, mul2, mul3, mul5, judge=multifilter.judge_all)))
     # [0, 30]
-"""
