a12c437861e28abd3cb43d7e3da1e52b440c6943
Dmitriy Kostinov <dkostinov@gmail.com>
2016-10-30 00:43:42 +0300

    .

--- 24464/stepik-512_24464-step5.py
+++ 24464/stepik-512_24464-step5.py
@@ -15,7 +15,7 @@ def primes():
 
 
 if __name__ == "__main__":
-    print(list(itertools.takewhile(lambda x: x <= 1000000, primes())))
+    print(list(itertools.takewhile(lambda x: x <= 31, primes())))
     # [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
 
 
