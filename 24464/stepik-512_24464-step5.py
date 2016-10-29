e9e123afcee2a00c49c2d24ff9538c0a143cd18b
Dmitriy Kostinov <dkostinov@gmail.com>
2016-10-30 00:43:09 +0300

    .

--- 24464/stepik-512_24464-step5.py
+++ 24464/stepik-512_24464-step5.py
@@ -2,14 +2,20 @@ import itertools
 
 
 def primes():
+    num = 2
+    yield num
     while True:
-        num = 2
-        yield num
         num += 1
+        flag = 1
+        for j in range(2, num-1):
+            if num % j == 0:
+                flag = 0
+        if flag:
+            yield num
 
 
 if __name__ == "__main__":
-    print(list(itertools.takewhile(lambda x: x <= 31, primes())))
+    print(list(itertools.takewhile(lambda x: x <= 1000000, primes())))
     # [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
 
 
