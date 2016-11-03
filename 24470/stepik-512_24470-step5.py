1c81d3f977de31a318b367a634d736e4bde8e7a0
Dmitriy Kos <dkostinov@gmail.com>
2016-11-03 17:09:39 +0200

    .

--- 24470/stepik-512_24470-step5.py
+++ 24470/stepik-512_24470-step5.py
@@ -45,7 +45,7 @@ string = "testtexttest"
 match = re.match(pattern, string)
 print(match)
 print(match.groups())
-"""
+
 pattern = r"Hello (abc|test)"
 string = "Hello abc"
 match = re.match(pattern, string)
@@ -53,3 +53,31 @@ print(match)
 print(match.group())
 print(match.group(0))
 print(match.group(1))
+
+pattern = r"(\w+)-\1"
+string = "test-test"
+match = re.match(pattern, string)
+print(match)
+print(match.groups())
+
+pattern = r"(\w+)-\1"
+string = "test-text"
+match = re.match(pattern, string)
+print(match)
+
+pattern = r"(\w+)-\1"
+string = "test-test chow-chow"
+duplicates = re.sub(pattern, r"\1", string)
+print(duplicates)
+"""
+
+pattern = r"(\w+)-\1"
+string = "test-test chow-chow"
+duplicates = re.findall(pattern, string)
+print(duplicates)
+
+pattern = r"((\w+)-\2)"
+string = "test-test chow-chow"
+duplicates = re.findall(pattern, string)
+print(duplicates)
+
