69906a4c5f14139fd2a4b5eaced1b9b7816063db
Dmitriy Kostinov <dkostinov@gmail.com>
2016-11-05 10:24:08 +0200

    .

--- 24470/stepik-512_24470-step8.py
+++ 24470/stepik-512_24470-step8.py
@@ -1,39 +1,25 @@
+import sys
 import re
 
-line = input()
-while line:
-    res = re.findall(r"\bcat\b", line)
-    if res and res[0] == 'cat':
+for line in sys.stdin:
+    line = line.rstrip()
+    res = re.search(r"\bcat\b", line)
+    if res:
         print(line)
-    line = input()
+
 
 """
-print(re.findall(r"\bcat\b", "catapult and cat"))
-print(re.findall(r"\bcat\b", "catcat"))
-print(re.findall(r"\bcat\b", "concat"))
-print(re.findall(r"\bcat\b", "Cat"))
-print(re.findall(r"\bcat\b", "\"cat\""))
-print(re.findall(r"\bcat\b", "!cat\?"))
-
-
-Вам дана последовательность строк. Выведите строки, содержащие "cat" в качестве слова.
-Примечание: Для работы со словами используйте группы символов \b и \B.
-Описание этих групп вы можете найти в документации.
-
 Sample Input:
-
-cat
-catapult and cat
-catcat
-concat
-Cat
-"cat"
-!cat?
-
+    cat
+    catapult and cat
+    catcat
+    concat
+    Cat
+    "cat"
+    !cat?
 Sample Output:
-
-cat
-catapult and cat
-"cat"
-!cat?
+    cat
+    catapult and cat
+    "cat"
+    !cat?
 """
