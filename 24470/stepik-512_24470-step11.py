56c7bfe3aaae2be901e9434c1f7ae516c8cedc8c
Dmitriy Kostinov <dkostinov@gmail.com>
2016-11-05 23:31:28 +0200

    .

--- 24470/stepik-512_24470-step11.py
+++ 24470/stepik-512_24470-step11.py
@@ -1,25 +1,18 @@
 import sys
 import re
 
-res = re.search(r"\b(\w+)\1\b", "blabla is a tandem repetition")
-print(res)
-res = re.search(r"\b(\w+)\1\b", "is a tandem blabla repetition")
-print(res)
-res = re.search(r"\b(\w+)\1\b", "123123 is good too")
-print(res)
-res = re.search(r"\b(\w+)\1\b", "go go")
-print(res)
-res = re.search(r"\b(\w+)\1\b", "aaa")
-print(res)
-
-
-
-"""for line in sys.stdin:
+for line in sys.stdin:
     line = line.rstrip()
-    res = re.search(r"\\", line)
+    res = re.search(r"\b(\w+)\1\b", line)
+
     if res:
         print(line)
+        print(res.groups())
+        print(res.group())
+        print(res.group(0))
+        print(res.group(1))
 
+"""
 Sample Input:
 
 blabla is a tandem repetition
