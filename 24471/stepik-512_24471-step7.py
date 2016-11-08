ff2bc70b97271dc274a5eb8c5c5dffb724cd420f
Dmitriy Kos <dkostinov@gmail.com>
2016-11-08 17:29:06 +0200

    .

--- 24471/stepik-512_24471-step7.py
+++ 24471/stepik-512_24471-step7.py
@@ -3,10 +3,24 @@ import re
 
 # link = input()
 link = "http://hosting.ukrtelecom.ua/stepik-512_24471-step7.html"
+# link = http://pastebin.com/raw/7543p0ns   #test3
 res = requests.get(link)
-print(res.text)
-print("- - -")
-print()
+# print(res.text)
+# print("- - -")
+pattern = re.compile(r"href=.+[',\"]")
+res1 = pattern.findall(res.text)
+# print(res1)
+for line in res1:
+    line = re.split(r"[',\"]", str(line))[1]
+    if line.startswith(".."):
+        continue
+    elif "//" in line:
+        result = re.search(r"//.+\b", line)
+        print(result)
+    else:
+        print(line)
+
+
 """
 Output
     mail.ru
