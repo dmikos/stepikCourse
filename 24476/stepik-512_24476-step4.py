197b99d111f276c8b6bda574c59a44927ab56ff2
Dmitriy Kos <dkostinov@gmail.com>
2016-11-14 17:19:44 +0200

    .

--- 24476/stepik-512_24476-step4.py
+++ 24476/stepik-512_24476-step4.py
@@ -27,12 +27,26 @@ token = j["token"]
 headers = {"X-Xapp-Token" : token}
 
 template = 'https://api.artsy.net/api/artists/{}'
-id = "4d8b92b34eb68a1b2c0003f4"
+# id = "4d8b92b34eb68a1b2c0003f4"
+res = dict()
+with open("dataset_24476_4.txt", "r") as f:
+    for line in f.readlines():
+        id = line.split()[0]
 
-# инициируем запрос с заголовком
-r = requests.get(template.format(id), headers=headers)
+        # инициируем запрос с заголовком
+        r = requests.get(template.format(id), headers=headers)
 
-# разбираем ответ сервера
-j = json.loads(r.text)
+        # разбираем ответ сервера
+        j = json.loads(r.text)
 
-print(j)
+        # res[j["birthday"]] = [j["sortable_name"]]
+        try:
+            res[j["birthday"]].append(j["sortable_name"])
+        except KeyError:
+            res[j["birthday"]] = [j["sortable_name"]]
+
+for i in sorted(res):
+    #print(i, res[i])
+    res[i].sort()
+    for j in res[i]:
+        print(j)
