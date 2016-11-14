3e4473f05f8ae0cd776448da787a99cb8091d78f
Dmitriy Kos <dkostinov@gmail.com>
2016-11-14 10:35:00 +0200

    .

--- 24473/stepik-512_24473-step4.py
+++ 24473/stepik-512_24473-step4.py
@@ -1,24 +1,24 @@
 import json
 
 result = dict()
-data_json = '[{"name": "A", "parents": []}, {"name": "B", "parents": ["A", "C"]}, {"name": "C", "parents": ["A"]}]'
-# data_json = input()
+# data_json = '[{"name": "A", "parents": []}, {"name": "B", "parents": ["A", "C"]}, {"name": "C", "parents": ["A"]}]'
+data_json = input()
 data = json.loads(data_json)
 for elem in data:
-    print(result)
+    # print(result)
     try:
-        result[elem["name"]] = +1
+        result[elem["name"]] += 1
     except KeyError:
         result[elem["name"]] = 1
-    print(result)
+    # print(result)
     if elem["parents"]:
         for parent in elem["parents"]:
             try:
                 result[parent] += 1
             except KeyError:
                 result[parent] = 1
-    print(result)
-    print("- - -")
+    # print(result)
+    # print("- - -")
 
 for i in sorted(result):
     print(i, " : ", result[i])
