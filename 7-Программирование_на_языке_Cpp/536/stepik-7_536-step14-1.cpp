3faeabfdc56cac597fb6b1495e7bb38a7f2a6816
Dmitriy Kos <dkostinov@gmail.com>
2016-11-21 16:37:20 +0200

    stepic.org - Программирование на языке C++

--- 7-Программирование_на_языке_Cpp/536/stepik-7_536-step14-1.cpp
+++ 7-Программирование_на_языке_Cpp/536/stepik-7_536-step14-1.cpp
@@ -5,10 +5,12 @@ int main()
   int i = 42;
   double d = 3.14;
   const char *s = "C-style string";
+  double D = true;
 
   std::cout << "This is an integer " << i << "\n";
   std::cout << "This is a double " << d << "\n";
   std::cout << "This is a \"" << s << "\"\n";
+  std::cout << "This is a BOOL type... \"" << D << "\"\n";
 
   return 0;
 }
